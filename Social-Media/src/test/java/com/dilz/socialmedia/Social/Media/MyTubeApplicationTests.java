668b3c95d122de279e43e74dfa3f746f6c8da414
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-08-29 13:24:23 +0530

    generate a project for create social media

---
+++ Social-Media/src/test/java/com/dilz/socialmedia/Social/Media/MyTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilz.socialmedia.Social.Media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
