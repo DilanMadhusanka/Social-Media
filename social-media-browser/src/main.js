4a1af32986ff254d3f4162f05517605692973e79
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-09-07 23:48:09 +0530

    render App component technical way in using main.js file

--- social-media-browser/src/main.js
+++ social-media-browser/src/main.js
@@ -2,8 +2,5 @@ import Vue from 'vue'
 import App from './App'
 
 new Vue({
-    render: function (createElement) {
-        return createElement(App)
-    },
-    el: '#app'
-})
+    render: h => h(App)
+}).$mount('#app')
