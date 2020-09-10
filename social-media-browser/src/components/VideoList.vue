a55f8cc7ed048fd90d92cfeeb13417c12b36fec5
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-09-10 14:29:01 +0530

    pass the video info as props from VideoList to VideoListItem component

--- social-media-browser/src/components/VideoList.vue
+++ social-media-browser/src/components/VideoList.vue
@@ -1,14 +1,17 @@
 <template>
   <ul>
-    Video List
-    {{videos.length}}
+    <VideoListItem v-for="(video, index) in videos" :key="index" v-bind:video="video" />
   </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
 export default {
   name: "VedioList",
   props: ["videos"],
+  components: {
+    VideoListItem,
+  },
 };
 </script>
 
