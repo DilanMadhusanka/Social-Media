01ce7919f37482e9c5e9ff874e218b4845f8e2b4
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-09-10 09:32:05 +0530

    install axios and do ajax request to youtube api and console print response

--- social-media-browser/src/App.vue
+++ social-media-browser/src/App.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script>
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+const API_KEY = "AIzaSyA4cN7hcMjsqIKgPVXhGErY6dF-miLDPc8";
 export default {
   name: "App",
   components: {
@@ -13,7 +15,18 @@ export default {
   },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        });
     },
   },
 };
