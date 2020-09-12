d24d581f1bb0b99c5610062a47550dd86599971f
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-09-12 11:43:32 +0530

    render the video player using iframe

--- social-media-browser/src/components/VideoDetails.vue
+++ social-media-browser/src/components/VideoDetails.vue
@@ -1,10 +1,33 @@
 <template>
-  <div v-if="video">{{video.snippet.title}}</div>
+  <div v-if="video">
+    <div class="embed-responsive embed-responsive-16by9">
+      <iframe class="embed-responsive-item" v-bind:src="videoUrl" frameborder="0" />
+    </div>
+    <div class="details">
+      <h4>{{video.snippet.title}}</h4>
+      <p>{{video.snippet.description}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "VideoDetails",
   props: ["video"],
+  computed: {
+    videoUrl() {
+      const { videoId } = this.video.id;
+      return `https://youtube.com/embed/${videoId}`;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.details {
+  margin-top: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+</style>
