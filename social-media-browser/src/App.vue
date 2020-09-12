69f9ba1abb565f0a67892a5b63d27424dd432021
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-09-12 12:02:03 +0530

    give the styles for show the video player and video list better

--- social-media-browser/src/App.vue
+++ social-media-browser/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container">
     <SearchBar v-on:termChange="onTermChange" />
-    <VideoDetails v-bind:video="selectedVideo" />
-    <VideoList v-on:videoSelect="onVideoSelect" v-bind:videos="videos" />
+    <div class="row">
+      <VideoDetails v-bind:video="selectedVideo" />
+      <VideoList v-on:videoSelect="onVideoSelect" v-bind:videos="videos" />
+    </div>
   </div>
 </template>
 
